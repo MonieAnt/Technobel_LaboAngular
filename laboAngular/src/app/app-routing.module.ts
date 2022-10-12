4d7dd8bd5a257f41efef7ed336b5d83651477a91
Monie Antoine - Full Stack Developer - Angular/React/NodeJS <monie.ant@gmail.com>
2022-10-12 10:47:03 +0200

    Mise en place de la structure Angular

--- laboAngular/src/app/app-routing.module.ts
+++ laboAngular/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-const routes: Routes = [];
+import { HomeComponent } from "./Home/home.component";
+
+const routes: Routes = [
+  // Home Page :
+  {path: "", component: HomeComponent},
+
+  // 404 Error Page :
+  {path: "404", component: ErrorPageComponent}
+
+  // Path Imbriqué :
+  // {path: "demo", children : [
+  //   {path: "2.1_compo", component}
+  // ]}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash : true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- laboAngular/src/app/app.module.ts
+++ laboAngular/src/app/app.module.ts
@@ -4,9 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Structure :
+import { HomeComponent } from './Home/home.component';
+import { NavbarComponent } from './Shared/navbar/navbar.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+// Autres
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
